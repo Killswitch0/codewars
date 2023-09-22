69c2bbd121017df8e8f9c69bf3f5d2f751671669
Killswitch0 <maximpanin1996@gmail.com>
2023-09-22 14:31:57 +0300

    Add So_many_permutations kata

---
+++ 4kyu/So_many_permutations/so_many_permutations.rb
@@ -0,0 +1,24 @@
+# In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+#
+#   Create as many "shufflings" as you can!
+#
+# Examples:
+#
+#   With input 'a':
+#                Your function should return: ['a']
+#
+#   With input 'ab':
+#              Your function should return ['ab', 'ba']
+#
+#   With input 'abc':
+#              Your function should return ['abc','acb','bac','bca','cab','cba']
+#
+#   With input 'aabb':
+#              Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+#
+#
+# Note: The order of the permutations doesn't matter.
+
+def permutations(string)
+  string.chars.permutation.map(&:join).uniq
+end
