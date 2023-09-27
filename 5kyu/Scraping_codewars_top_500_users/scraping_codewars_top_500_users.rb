8b3baa4bbe89659d5f1dc01096760921659e9bd6
Killswitch0 <maximpanin1996@gmail.com>
2023-09-27 16:35:34 +0300

    Upd

--- 5kyu/Scraping_codewars_top_500_users/scraping_codewars_top_500_users.rb
+++ 5kyu/Scraping_codewars_top_500_users/scraping_codewars_top_500_users.rb
@@ -1,3 +1,28 @@
+# DESCRIPTION:
+#   You should get and parse the html of the codewars leaderboard page.
+#
+#     You can use Nokogiri(Ruby) or BeautifulSoup(Python) or CheerioJS(Javascript).
+#
+#     For Javascript: Return a Promise resolved with your 'Leaderboard' Object!
+#
+# Note: Use the Overall leaderboard table.
+#   Return a 'Leaderboard' object with a position property.
+#   Leaderboard#position should contain 500 'User' objects.
+#
+#
+# Leaderboard#position[i] should return the ith ranked User(1 based index).
+#   Each User should have the following properties
+#   User#name    # => the user's username, not their real name
+#   User#clan    # => the user's clan, empty string if empty clan field
+#   User#honor   # => the user's honor points as an integer
+#
+#
+# Ex:
+#   an_alien = leaderboard.position[3]   # => #<User:0x3124da0 @name="myjinxin2015", @clan="China Changyuan", @honor=21446>
+#   an_alien.name                        # => "myjinxin2015"
+#   an_alien.clan                        # => "China Changyuan"
+#   an_alien.honor                       # => 21446
+
 require 'nokogiri'
 require 'open-uri'
 
@@ -46,4 +71,4 @@ end
 leaderboard = solution
 
 leaderboard.position.size # 500
-leaderboard.position[1].name 
+leaderboard.position[1].name
