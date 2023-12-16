7b7eaa181db5b9384c0280b3b2161d01ab952d17
Max <coachviktorovich@gmail.com>
2023-12-16 16:53:27 +0200

    refactor break_camel_case kata

--- 6kyu/Break_camel_Case/break_camel_case.rb
+++ 6kyu/Break_camel_Case/break_camel_case.rb
@@ -1,17 +1,21 @@
+# Complete the solution so that the function will break up camel casing, using a space between words.
+
+# Example
+#   "camelCasing"  =>  "camel Casing"
+#   "identifier"   =>  "identifier"
+#   ""             =>  ""
+
 def solution(string)
     result = []
     
     string.chars.each_with_index do |l, i|
-      if string[i + 1].nil?
-        result << l
-        break
-      end 
-      
-      if l == l.downcase && string[i + 1] == string[i + 1].upcase
-        result << l + ' ' 
-      else
-        result << l
-      end 
+      next_let = string[i + 1]  
+      result << l
+
+      break if next_let.nil?
+
+      result << ' ' if l == l.downcase && 
+                        next_let == next_let.upcase
     end
     
     result.join
